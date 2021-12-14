c27228714af27be334e3d8c8dad8ff05a1d0c45e
tlqiao <qiaotl@163.com>
2021-12-14 09:38:12 +0800

    update it

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,7 +3,8 @@ module.exports ={
         headless: false,
         slowMo: 150,
         args:['--window-size=1920,1080'],
-        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+        // executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
         devtools: false
     },
     browserContext : "default",
